1a692b7c1f5dbca2585f76f3345cb0ea12eff681
Yura <kubishka.yur@gmail.com>
2020-02-15 23:56:41 +0200

    Made some changes

--- IdeaProjects/Lab(OOP)/src/com/company/Main.java
+++ IdeaProjects/Lab(OOP)/src/com/company/Main.java
@@ -3,22 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        long a = 1, b = 1, n = 2, m = 2, sum = 0;
-        for (long i = a; i <= n; i++) {
-            for (long j = b;j <= m; j++) {
-                long operation = 0;
+        long a = 1, b = 1, n = 1, m = 2, sum = 0;
+        for (long i = a; i <= n ; i++) {
+            if (i - 2 == 0) {
+                System.out.println("ZeroDivisionError");
+                System.exit(1);
+            }
+            for (long j = b; j <= m; j++) {
                 if (i * j == 0) {
-                    System.out.println("При виконанні завдання виникла помилка, пропускаємо");
-                    continue;
-                } else if (i - 2 == 0) {
-                    System.out.println("ZeroDivisionError");
-                    System.out.println("При виконанні завдання виникла помилка ділення на 0, операція невизначена, пропуск (((");
-                    continue;
+                    System.out.println("Так як дріб, чисельник якого 0, а знаменник відмінний від 0, рівний нулю, то поточний доданок - 0");
                 }
-                else{
-                    operation = (i * j) / (i - 2);
-                    sum += operation;
-                }
+                sum += (double)(i * j) / (i - 2);
             }
         }
         System.out.println(sum);
